fb75eb2c809787a9454cafb64c584d27af3c8e79
adamc <adamcopeland@protonmail.com>
2018-08-23 15:17:29 -0400

    Update adam.py

--- adam.py
+++ adam.py
@@ -1,24 +1,24 @@
 #ADAM
 
 import random
-answers = ['Did you try turning it off and on?', 'Blame Networking.', 'Have you tried Googling the error?', 
+answers = ['Did you try turning it off and on?', 'Blame Networking.', 'Have you tried Googling the error code?', 
            'Try reading the documentation.', 'Take a short break and try again.', 
            'Get some coffee and try again.', 'Maybe you\'re hungry? Take a break.', 'Try meditation.', 
            'Unplug the machine.','Delete everything on the machine.', 'Wipe the hard drive.', 
-           'Find an open window and throw the machine out of it.', 'It works better hacked',
-           'Sorry, I\'m on vacation in Flavortown right now.', 'Security isn\'t that important, don\'t worry.'
+           'Find an open window and throw the machine out of it.', 'It works better hacked.',
+           'Sorry, I\'m on vacation in Flavortown right now.', 'Security isn\'t that important, don\'t worry.',
            'I know it hasn\'t worked yet, but if you try 10 or more times it might work', 
-           'Please open up a ticket and forward it to Dr. Cole', 
+           'Please open up a ticket and forward it to the president', 
            'Go to the bathroom, create a giant wad of toilet paper and scream all of your worries into it at the top of your lungs.',
-           'Adam is not here to answer your question but his consulting fees start at $200 an hour.',
            'Sorry, my ability to help you is commensurate with your experience.',
            'Flavortown is near.', 'It\'s "Bomgar", not "Bombguard"',
            'Let me run this up the flagpole to management and see what they think.',
-           'Waiting for leadership...', 'I don\'t have any cycles left to solve that problem.']
+           'Waiting for leadership...', 'I don\'t have any cycles left to solve that problem.',
+           'Restart the domain controllers']
 
 print('\nHello, I am A.D.A.M., The Automated Disaster Answering Machine.')
 
-def Magic8Ball():
+def ADAM():
     print('\nAsk me a question.\n')
     print('> ', end='')
     input()
@@ -29,12 +29,12 @@ def Replay():
     print ('Do you have another question? [Y/N] ', end='')
     reply = input()
     if reply == 'Y' or reply == 'y':
-        Magic8Ball()
+        ADAM()
     elif reply == 'N' or reply == 'n':
         exit()
     else:
-        print('I apologies, I did not catch that. Please repeat.')
+        print('My apologies, I did not catch that. Please try again.')
         Replay()
 
 		
-Magic8Ball()
+ADAM()
