26bce163094f7005dcfe608a923e688bd648fd36
Adam Copeland <lesservehicle@protonmail.com>
2019-09-23 14:36:04 -0400

    Update adam.py
    pie

--- adam.py
+++ adam.py
@@ -1,17 +1,17 @@
 #ADAM
 
-from __future__ import print_function
 import random
+from builtins import input
 import os
 
-answers = ['Did you try turning it off and on?', 'Blame Networking.', 'Have you tried Googling the error code?', 
-           'Try reading the documentation.', 'Take a short break and try again.', 
-           'Get some coffee and try again.', 'Maybe you\'re hungry? Take a break.', 'Try meditation.', 
-           'Unplug the machine.','Delete everything on the machine.', 'Wipe the hard drive.', 
+answers = ['Did you try turning it off and on?', 'Blame Networking.', 'Have you tried Googling the error code?',
+           'Try reading the documentation.', 'Take a short break and try again.',
+           'Get some coffee and try again.', 'Maybe you\'re hungry? Take a break.', 'Try meditation.',
+           'Unplug the machine.','Delete everything on the machine.', 'Wipe the hard drive.',
            'Find an open window and throw the machine out of it.', 'It works better hacked.',
            'Sorry, I\'m on vacation in Flavortown right now.', 'Security isn\'t that important, don\'t worry.',
-           'I know it hasn\'t worked yet, but if you try 10 or more times it might work', 
-           'Please open up a ticket and forward it to the president', 
+           'I know it hasn\'t worked yet, but if you try 10 or more times it might work',
+           'Please open up a ticket and forward it to the president',
            'Go to the bathroom, create a giant wad of toilet paper and scream all of your worries into it at the top of your lungs.',
            'Sorry, my ability to help you is commensurate with your experience.',
            'Flavortown is near.', 'It\'s "Bomgar", not "Bombguard"',
@@ -29,12 +29,12 @@ def ADAM():
     input()
     print ('\n' + answers[random.randint(0, len(answers)-1)] + '\n')
     Replay()
-    
+
 def Replay():
     print ('Do you have another question? [Y/N] ', end='')
     reply = input()
     if reply == 'Y' or reply == 'y':
-	_=os.system("cls")
+        _=os.system("cls")
         _=os.system("clear")
         ADAM()
     elif reply == 'N' or reply == 'n':
@@ -43,5 +43,5 @@ def Replay():
         print('My apologies, I did not catch that. Please try again.')
         Replay()
 
-		
+
 ADAM()
